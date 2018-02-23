feb7fdeb1021874d49db059f95a5d3d2c2ce92b2
AXAz0r <thealeksaradovic@gmail.com>
2018-02-23 14:49:56 +0100

    Something That Actually Works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-mod sonata;
-use sonata::config::SonataConfig;
+#[macro_use] extern crate serde_derive;
+extern crate serde_yaml;
+
+pub mod sonata;
+use sonata::config;
 
 fn main() {
-    let snt = SonataConfig::new("SIGMA_LOCATION");
+    let cfg: config::Configuration = config::Configuration::new("/home/alex/GitHub/apex-sigma-core".to_owned());
+    println!("Token: {}", cfg.dsc.get_token());
 }
--- src/sonata/config.rs
+++ src/sonata/config.rs
@@ -1,61 +1,53 @@
-extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
 use std::fs::File;
-use std::io::prelude;
+use std::path::Path;
+use std::process::exit;
+use serde_yaml;
 
-fn getYamlContent(location: str) -> str {
-    let mut yaml_file = File::open(location)?;
-    let mut yaml_cont = String::new();
-    yaml_file.read_to_string(&mut yaml_cont)?
+#[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
+#[serde(default)]
+pub struct DiscordConfiguration {
+    token: String,
+    pub bot: bool,
 }
 
-struct SonataConfig {
-    sigma_location: str
-}
-
-impl SonataConfig {
-    pub fn new(location: str) -> SonataConfig {
-        SonataConfig {sigma_location: location}
+impl DiscordConfiguration {
+    fn new(cfgfile: &Path) -> DiscordConfiguration {
+        let file = File::open(cfgfile).expect("Failed for find the discord configuration file.");
+        let dsc_cfg: DiscordConfiguration = serde_yaml::from_reader(&file).expect("Failed to read discord configuration file.");
+        return dsc_cfg;
+    }
+    pub fn get_token(self) -> String {
+        self.token
     }
 }
 
-struct DiscordConfig {
-    token: str,
-    owners: [i64],
-    bot: bool
+pub struct Configuration {
+    pub dsc: DiscordConfiguration,
 }
 
-impl DiscordConfig {
-    pub fn load(snt: SonataConfig) -> DiscordConfig {
-        let config_location = snt::sigma_location + "/config/core/discord.yml";
-        let config_yaml = getYamlContent(config_location);
-        let configuration = YamlLoader::load_from_str(config_yaml)::unwrap();
+impl Configuration {
+    fn check_config_files(db_path: &Path, dsc_path: &Path, pref_path: &Path) {
+        let mut missing = false;
+        if !db_path.exists() {
+            missing = true;
+        } else if !dsc_path.exists() {
+            missing = true;
+        } else if !pref_path.exists() {
+            missing = true
+        }
+        if missing {
+            println!("Missing configuration file.");
+            exit(1);
+        }
     }
-}
-struct DatabaseConfig {
-    database: str,
-    auth: bool,
-    host: str,
-    port: i32,
-    username: str,
-    password: str
-}
-
-struct PreferencesConfig {
-    dev_mode: bool,
-    status_rotation: bool,
-    prefix: str,
-    currency: str,
-    currency_icon: str,
-    website: str,
-    text_only: str,
-    music_only: str,
-    dscbots_token: str,
-    movelog_channel: i64
-}
-
-struct Configuration {
-    dsc: DiscordConfig,
-    db: DatabaseConfig,
-    pref: PreferencesConfig
+    pub fn new(location: String) -> Configuration {
+        let db_loc = format!("{}/config/core/database.yml", location);
+        let dsc_loc = format!("{}/config/core/discord.yml", location);
+        let pref_loc = format!("{}/config/core/preferences.yml", location);
+        let db_path = Path::new(&db_loc);
+        let dsc_path = Path::new(&dsc_loc);
+        let pref_path = Path::new(&pref_loc);
+        Configuration::check_config_files(db_path, dsc_path, pref_path);
+        Configuration {dsc: DiscordConfiguration::new(dsc_path)}
+    }
 }
